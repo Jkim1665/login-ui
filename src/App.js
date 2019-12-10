191f854e57956d14b0b8cb4cf885484a74f5b146
Jin Kim <jkim1665@gmail.com>
2019-12-10 12:27:31 -0500

    added an input box

--- src/App.js
+++ src/App.js
@@ -4,12 +4,35 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return ( 
+    return (
       <div className="App">
+
         <div className="App__Aside"></div>
-        <div className="App__Form"></div>
-       
-     </div>
+
+        <div className="App__Form">
+
+          <div className="PageSwitcher">
+
+            <a href="#" className="PageSwitcher__Item">Sign In</a>
+
+            <a href="#" className="PageSwitcher__Item PageSwitcher__Item--Active">Sign Up</a>
+          </div>
+
+          <div className="FormTitle">
+            <a href="#" className="FormTitle__Link">Sign In</a> or <a href="#"
+              className="FormTitle__Link FormTitle__Link--Active">Sign Up</a>
+          </div>
+
+          <div className="FormCenter">
+            <form className="FormFields" onSubmit={this.handleSubmit}>
+              <div className="FormField">
+                <label className="FormField__Label" htmlFor="name">Full Name</label>
+                <input type="text" id="name" className="FormField__Input" placeholder="Enter your full name" name="name" />
+              </div>
+              </form>
+          </div>
+        </div>
+      </div>
     )
   }
 }
